717652a680f974e5690d6e9412841f9d63d4c967
Brage Roalkvam <brage89@gmail.com>
2014-10-16 15:59:35 +0200

    one train moving

--- RailwaySim.m
+++ RailwaySim.m
@@ -3,10 +3,18 @@
 clear all; clc;
 
 global global_info; %user data
-global_info.PRINT_LOOP_NUMBER = 1;
-global_info.STOP_AT = 50;
+% 24h of simultion
+global_info.START_AT = [0 0 0]; % OPTION: start simulations at 10 AM
+global_info.STOP_AT  = [23 59 59]; % OPTION: stop  simulations at 15 AM
+global_info.DELTA_TIME = 60;  % delta_T is 1 minutes
 
-pns = pnsstructur('RailwaySim_pdf');
+png = pnstruct('RailwaySim_pdf');
 
-dyn.m0 = {};
-dyn.ft = {};
+dyn.m0 = {'pS45', 2};%How many tokens that are in places
+dyn.ft = {'t44S', [5 01 0]}; %firering time [hh mm ss]
+
+pni = initialdynamics(pns, dyn);
+
+sim = gpensim(pni);
+prnss(sim);
+plotp(sim, {'pS45'});
---
+++ RailwaySim_pdf.m
@@ -0,0 +1,70 @@
+% Defining Stations and transissions
+% Places are the Stations
+% Transsisions are railways between stations
+% Stavanger - Sandnes
+
+function [png] = RailwaySim_pdf()
+png.PN_name = 'Railway simulation - Stavanger to Sandnes';
+png.set_of_Ps = {'pS39', 'pS40', 'pS41', 'pS42', 'pS43', 'pS44', 'pS45'};
+png.set_of_Ts = {'t39S', 't39N', 't40S', 't40N', 't41S', 't41N', 't42S', 't42N', 't43S', 't43N', 't44S', 't44N'};
+png.set_of_As = { %North bound
+                  'pS39', 't39N', 1, 't39N', 'pS40', 1,...
+                  'pS40', 't40N', 1, 't40N', 'pS41', 1,...
+                  'pS41', 't41N', 1, 't41N', 'pS42', 1,...
+                  'pS42', 't42N', 1, 't42N', 'pS43', 1,...
+                  'pS43', 't43N', 1, 't43N', 'pS44', 1,...
+                  'pS44', 't44N', 1, 't44N', 'pS45', 1,...
+                  'pS45', 't44S', 1, 't44S', 'pS44', 1,...
+                  'pS44', 't43S', 1, 't43S', 'pS43', 1,...
+                  'pS43', 't42S', 1, 't42S', 'pS42', 1,...
+                  'pS42', 't41S', 1, 't41S', 'pS41', 1,...
+                  'pS41', 't40S', 1, 't40S', 'pS40', 1,...
+                  'pS40', 't39S', 1, 't39S', 'pS39', 1
+
+                  };
+
+% 1. Gulskogen
+% 2. Mjøndalen
+% 3. Steinberg
+% 4. Hokksund
+% 5. Vestfossen
+% 6. Darbu
+% 7. Kongsberg
+% 8. Nordagutu
+% 9. Bø
+% 10. Lunde
+% 11. Drangedal
+% 12. Neslandsvatn
+% 13. Gjerstad
+% 14. Vegårshei
+% 15. Nelaug
+% 16. Vennesla
+% 17. Kristiansand
+% 18. Nodeland
+% 19. Breland
+% 20. Marnardal
+% 21. Audnedal
+% 22. Snartemo
+% 23. Storekvina
+% 24. Gyland
+% 25. Sira
+% 26. Moi
+% 27. Egersund
+% 28. Hellvik
+% 29. Sirevåg
+% 30. Ogna
+% 31. Brusand
+% 32. Vigrestad
+% 33. Varhaug
+% 34. Nærbø
+% 35. Bryne
+% 36. Klepp
+% 37. Øksnavadporten
+% 38. Ganddal
+% 39. Sandnes
+% 40. Sandnes sentrum
+% 41. Gausel
+% 42. Jåttåvågen
+% 43. Mariero
+% 44. Paradis
+% 45. Stavanger
