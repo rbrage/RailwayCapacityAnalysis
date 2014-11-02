beb4828e57e80bb8ad042a4120b1ef21706068c6
Heine Furubotten <hfurubotten@gmail.com>
2014-11-02 22:37:10 +0100

    Added correct trasition exit

--- RailwaySim.m
+++ RailwaySim.m
@@ -11,10 +11,11 @@ global_info.DELTA_TIME = 60;  % delta_T is 1 minutes
 pns = pnstruct('RailwaySim_pdf');
 
 dyn.m0 = {'Stavanger', 2};%How many tokens that are in places
-dyn.ft = {'allothers', [00 10 00]}; %firering time [hh mm ss]
+dyn.ft = {'allothers', [01 00 00]}; %firering time [hh mm ss]
 
 pni = initialdynamics(pns, dyn);
 
 sim = gpensim(pni);
 prnss(sim);
-plotp(sim, {'Stavanger', 'Sandnes'});
+plotp(sim, {'Stavanger', 'Paradis', 'Mariero', 'Jattavagen',...
+    'Gausel', 'SandnesSentrum', 'Sandnes'});
