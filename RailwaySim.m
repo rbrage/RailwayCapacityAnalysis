bae8622a1e50d54e0ec7a173ad64b83728d62214
Brage Roalkvam <brage89@gmail.com>
2014-11-03 17:48:14 +0100

    add strat on trains from sandnes

--- RailwaySim.m
+++ RailwaySim.m
@@ -8,13 +8,15 @@ global_info.START_AT = [04 59 00]; % OPTION: start simulations at 10 AM
 global_info.STOP_AT  = [05 32 00]; % OPTION: stop  simulations at 15 AM
 %global_info.STOP_AT  = 3;
 global_info.DELTA_TIME = 60;  % delta_T is 1 minutes
-global_info.next_train_time = [[05 00 00];[05 16 00];[05 31 00]];
+global_info.next_train_time_Stavanger = [[05 00];[05 16];[05 31]];
+%global_info.next_train_time_Sandnes = [[05 00];[05 16];[05 31]];
 global_info.next_train_type = [double('N') double('S')];
-initokens = length(global_info.next_train_time);
+initokensStavanger = length(global_info.next_train_time_Stavanger);
+%initokensSandnes = length(global_info.next_train_time_Sandnes);
 
 pns = pnstruct('RailwaySim_pdf');
 
-dyn.m0 = {'pGenStavanger', initokens};%How many tokens that are in places
+dyn.m0 = {'pGenStavanger', initokensStavanger};%, 'pGenSandnes', initokensSandnes};%How many tokens that are in places
 dyn.ft = {'allothers', [00 01 00]}; %firering time [hh mm ss]
 
 pni = initialdynamics(pns, dyn);
--- tInStavanger_pre.m
+++ tInStavanger_pre.m
@@ -2,21 +2,18 @@ function [fire, transition] = tInStavanger_pre(transition)
 
 global global_info;
 
-if isempty(global_info.next_train_time),
+if isempty(global_info.next_train_time_Stavanger),
 
     fire = 0; return;
 
 end;
 
 ctime = string_HH_MM_SS(current_time());%current_time();
-NTIT = global_info.next_train_time();
+NTIT = global_info.next_train_time_Stavanger();
 nttime = string_HH_MM_SS((NTIT(1,1)*60*60)+(NTIT(1,2)*60));
-disp(ctime);
-disp(nttime);
 
-disp(global_info.next_train_time);
 if eq(ctime, nttime),
-  global_info.next_train_time = global_info.next_train_time(2:end,:);
+  global_info.next_train_time_Stavanger = global_info.next_train_time_Stavanger(2:end,:);
   trainType = global_info.next_train_type(2);
   transition.new_color = {char(trainType)};
   fire = 1;
