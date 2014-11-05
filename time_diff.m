155709586ec4e721ae96a357379b4db7c9e5cd2e
Heine Furubotten <hfurubotten@gmail.com>
2014-11-05 04:23:28 +0100

    Function to find time difference

---
+++ time_diff.m
@@ -0,0 +1,11 @@
+% Finds the difference between values in a array
+% Values in array have to be in military integer time.
+function [diff] = time_diff(times)
+
+len = length(times);
+diff = zeros(1,len-1);
+for i = 1:len-1,
+   diff(i) = convert_militery_time(times(i + 1), 2) - convert_militery_time(times(i), 2);
+end;
+    
+    
