ea967811863dcdbadc0fcb2810aaeb0a3c3d2aec
Brage Roalkvam <brage89@gmail.com>
2014-11-05 15:09:27 +0100

    Change STo.. to STFrom

--- RailwaySim_pdf.m
+++ RailwaySim_pdf.m
@@ -19,9 +19,9 @@ png.set_of_As = {'pGenStavanger','tInStavanger', 1, 'tInStavanger', 'StavangerS'
 for i = 1:length(global_info.stations),
     png.set_of_Ps = [png.set_of_Ps {strjoin(['', global_info.stations(i)], '')}];
     if i < length(global_info.stations),
-        png.set_of_Ts = [png.set_of_Ts {strjoin(['STo', global_info.stations(i+1)], '')}];
-        png.set_of_As = [png.set_of_As {strjoin(['', global_info.stations(i)], ''), strjoin(['STo', global_info.stations(i+1)], '')} 1 ...
-            {strjoin(['STo', global_info.stations(i+1)], ''), strjoin(['', global_info.stations(i+1)], '') 1}];
+        png.set_of_Ts = [png.set_of_Ts {strjoin(['SFrom', global_info.stations(i)], '')}];
+        png.set_of_As = [png.set_of_As {strjoin(['', global_info.stations(i)], ''), strjoin(['SFrom', global_info.stations(i)], '')} 1 ...
+            {strjoin(['SFrom', global_info.stations(i)], ''), strjoin(['', global_info.stations(i+1)], '') 1}];
     end;
 end;
 
