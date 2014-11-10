0d225833d5284d9beb015722143c2b3bca420418
Brage Roalkvam <brage89@gmail.com>
2014-11-10 14:21:57 +0100

    change the forlder name, and add regional trains time

--- RailwaySim.m
+++ RailwaySim.m
@@ -8,12 +8,18 @@ global_info.START_AT = [04 30 00]; % OPTION: start simulations at 10 AM
 global_info.STOP_AT  = [26 59 59]; % OPTION: stop  simulations at 15 AM
 
 global_info.DELTA_TIME = 60;  % delta_T is 1 minutes
-global_info.times_rogaland_south = dlmread('Stavanger_Egersund_traintimes.txt', '\t', 0, 1);%dlmread('test.txt', '\t', 0, 1);% %dlmread('test.txt', '\t', 0, 1);
-global_info.times_rogaland_north = dlmread('Egersund_Stavanger_traintimes.txt', '\t', 0, 1);%dlmread('test2.txt', '\t', 0, 1);% %dlmread('test2.txt', '\t', 0, 1);
+%Local train times
+global_info.times_rogaland_south = dlmread('/db/Stavanger_Egersund_traintimes.txt', '\t', 0, 1);%dlmread('test.txt', '\t', 0, 1);% %dlmread('test.txt', '\t', 0, 1);
+global_info.times_rogaland_north = dlmread('/db/Egersund_Stavanger_traintimes.txt', '\t', 0, 1);%dlmread('test2.txt', '\t', 0, 1);% %dlmread('test2.txt', '\t', 0, 1);
+%Regional train times
+%global_info.times_regional_south = dlmread('/db/Stavanger_Drammen_traintimes.txt', '\t', 0, 1);
+%global_info.times_regional_south = dlmread('/db/Drammen_Stavanger_traintimes.txt', '\t', 0, 1);
+
 global_info.last_route_traveled_North = 0;
 global_info.last_route_traveled_South = 0;
+
 global_info.next_train_type = {'North' 'South'};
-[global_info.stations, global_info.traksnorth, global_info.trakssouth, global_info.stationtracks] = textread('Trainstations.txt', '%s %d %d %d');
+[global_info.stations, global_info.traksnorth, global_info.trakssouth, global_info.stationtracks] = textread('/db/Trainstations.txt', '%s %d %d %d');
 
 initokensStavanger = size(global_info.times_rogaland_south(1, 1:end),2);
 initokensSandnes = size(global_info.times_rogaland_north(13,1:end),2);
