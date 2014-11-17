f50bab62ef69f090745b43654869878679ab3a8b
Heine Furubotten <hfurubotten@gmail.com>
2014-11-17 05:31:16 +0100

    Overcapasity registered and written to file

--- COMMON_POST.m
+++ COMMON_POST.m
@@ -2,14 +2,17 @@ function [] = COMMON_POST(transition)
 
 global global_info;
 
-%release all resources used by transition
+%% release all resources used by transition
 release(transition.name);
+
+%% filters non processable transitions
 if ismember(transition.name, {'tInStavanger','tInSandnes','tInNaerbo',...
         'tInEgersund', 'tInMoi', 'tInKristiansand', 'tInDrammen','tOutStavangerS',...
         'tOutSandnes','tOutNaerbo','tOutEgersund','tOutKristiansand','tOutDrammen', 'tInGanddal', 'tInGulskogen', 'tOutGulskogen', 'tOutGanddal'}),
     return;
 end;
 
+%% gets transition info
 direction = char(transition.name(1));
 ctime = mod(current_time(), 24*60*60);
 station = transition.name(3:end);
@@ -22,6 +25,15 @@ else
     to_station = char(global_info.stations(global_info.stationnr(station) + 1));
 end;
 
+%% Checks for over capasity on station
+place = get_place(to_station);
+if place.tokens > global_info.station_tracks(to_station),
+    fid = fopen('results/overcapasity.txt', 'a');
+    fprintf(fid, '%s\t%d\t%d\t%s\n', string_HH_MM_SS(ctime), place.tokens, global_info.station_tracks(to_station), to_station);
+    fclose(fid);
+end;
+
+%% general out reporting
 tokID = tokenAny(to_station, 1);
 colors = get_color(to_station, tokID); % colors(routenr, 'N' or 'S')
 routnr = str2num(colors{1});
--- helpers/init_results_files.m
+++ helpers/init_results_files.m
@@ -2,27 +2,39 @@ function [] = init_results_files()
 fid = fopen('results/run.txt', 'w');
 fprintf(fid, '%s\t%s\t%s\t%s\n', 'Time', 'A/D' , 'Direction', 'Type', 'Routnr', 'Schedule', 'Station');
 fclose(fid);
+
 fid = fopen('results/run_south.txt', 'w');
 fprintf(fid, '%s\t%s\t%s\t%s\t%s\t%s\n', 'Time', 'A/D' , 'Direction', 'Type', 'Routnr', 'Schedule', 'Station');
 fclose(fid);
+
 fid = fopen('results/run_north.txt', 'w');
 fprintf(fid, '%s\t%s\t%s\t%s\t%s\t%s\n', 'Time', 'A/D' , 'Direction', 'Type', 'Routnr', 'Schedule', 'Station');
 fclose(fid);
+
 fid = fopen('results/delays.txt', 'w');
 fprintf(fid, '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n', 'Time', 'Schedule', 'Direction', 'Type', 'Routnr', 'From Station', 'To Station','Transition');
 fclose(fid);
+
 fid = fopen('results/tGenStavanger.txt', 'w');
 fprintf(fid, '%s\t%s\t%s\t%s\t%s\t%s\t%s\n', 'Time', 'GENIN', 'Direction', 'TrainType', 'StopPlace', 'Time', 'Routenr');
 fclose(fid);
+
 fid = fopen('results/tGenKristiansand.txt', 'w');
 fprintf(fid, '%s\t%s\t%s\t%s\t%s\t%s\t%s\n', 'Time', 'GENIN', 'Direction', 'TrainType', 'StopPlace','Time', 'Routenr');
 fclose(fid);
+
 fid = fopen('results/tGenDrammen.txt', 'w');
 fprintf(fid, '%s\t%s\t%s\t%s\t%s\t%s\t%s\n', 'Time', 'GENIN', 'Direction', 'TrainType', 'StopPlace','Time', 'Routenr');
 fclose(fid);
+
 fid = fopen('results/tGenMoi.txt', 'w');
 fprintf(fid, '%s\t%s\t%s\t%s\t%s\t%s\t%s\n', 'Time', 'GENIN', 'Direction', 'TrainType', 'StopPlace','Time', 'Routenr');
 fclose(fid);
+
 fid = fopen('results/tGenFeigth.txt', 'w');
 fprintf(fid, '%s\t%s\t%s\t%s\t%s\n', 'Time', 'GENIN', 'Direction', 'TrainType', 'StopPlace');
+fclose(fid);
+
+fid = fopen('results/overcapasity.txt', 'w');
+fprintf(fid, '%s\t%s\t%s\t%s\n', 'Time', 'Current trains', 'Max trains', 'Place');
 fclose(fid);
